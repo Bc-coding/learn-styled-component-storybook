4d340cf27aafdef439f77fc2fb24ef70c9cbd139
BCarrie <bcarrie@premierfarnell.com>
2022-10-18 14:36:10 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,39 +1,16 @@
-import Example1 from "./components/Example1";
-import Example2 from "./components/Example2";
-import Example3 from "./components/Example3";
-import Example4 from "./components/Example4";
-import Example5 from "./components/Example5";
-import Example7 from "./components/Example7";
-import Example6 from "./components/Example6";
-import Example9 from "./components/Example9";
-import Example8 from "./components/Example8";
-import Example10 from "./components/Example10";
-import Example11 from "./components/Example11";
-import Example13 from "./components/Example13";
-import Example12 from "./components/Example12";
-import Example14 from "./components/Example14";
-import Example15 from "./components/Example15";
+import StyledComponents from "./components/styledComponents";
+import Task from "./components/task/Task";
 
-function App() {
+export default function App() {
+  const task = {
+    id: "1",
+    title: "first task",
+    state: "default",
+  };
   return (
     <div className="App">
-      <Example1 />
-      <Example2 />
-      <Example3 />
-      <Example4 />
-      <Example5 />
-      <Example6 />
-      <Example7 />
-      <Example8 />
-      <Example9 />
-      <Example10 />
-      <Example11 />
-      <Example12 />
-      <Example13 />
-      <Example14 />
-      <Example15 />
+      <StyledComponents />
+      <Task task={task} />
     </div>
   );
 }
-
-export default App;
---
+++ src/components/styledComponents/index.js
@@ -0,0 +1,37 @@
+import Example1 from "./Example1";
+import Example2 from "./Example2";
+import Example3 from "./Example3";
+import Example4 from "./Example4";
+import Example5 from "./Example5";
+import Example6 from "./Example6";
+import Example7 from "./Example7";
+import Example8 from "./Example8";
+import Example9 from "./Example9";
+import Example10 from "./Example10";
+import Example11 from "./Example11";
+import Example12 from "./Example12";
+import Example13 from "./Example13";
+import Example14 from "./Example14";
+import Example15 from "./Example15";
+
+export default function StyledComponents() {
+  return (
+    <div>
+      <Example1 />
+      <Example2 />
+      <Example3 />
+      <Example4 />
+      <Example5 />
+      <Example6 />
+      <Example7 />
+      <Example8 />
+      <Example9 />
+      <Example10 />
+      <Example11 />
+      <Example12 />
+      <Example13 />
+      <Example14 />
+      <Example15 />
+    </div>
+  );
+}
